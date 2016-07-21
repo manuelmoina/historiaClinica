31997c075c1afbab68563fec6d72c9a002103ef0
emoina <emoina@eduardo>
2016-07-21 12:17:20 -0500

    ...

---
+++ HistoriaClinicaCBDMQ/HistoriaClinicaCBDMQ-war/src/java/beans/logicaBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author emoina
+ */
+@ManagedBean
+@SessionScoped
+public class logicaBean {
+
+   
+    
+    public logicaBean() {
+    }
+    
+    public String  autentificacion(){
+    
+        return "";
+    }
+}
